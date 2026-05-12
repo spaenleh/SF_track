ad2237b574566b942d52b2840b1462fc4f5f7655
spaenleh <spaenleh@gmail.com>
2026-05-12 09:57:46 +0200

    feat: add users with roles and auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :track, :scopes,
+  user: [
+    default: true,
+    module: Track.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Track.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :track,
   ecto_repos: [Track.Repo],
   generators: [timestamp_type: :utc_datetime]
